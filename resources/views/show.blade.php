990acb6d718e18ae12950c1946f97ed62522e6d8
José <Jos>
2024-02-08 17:33:59 +0100

    V2

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,6 +3,7 @@
 @section('contenido')
 <h1>Ficha de "{{ $post->titulo }}"</h1>
 <ul class="list-group">
+  <li class="list-group-item">Autor: {{$post->usuario->login}}</li>
   <li class="list-group-item">Contenido: {{ $post->contenido}}</li>
   <li class="list-group-item">Fecha de creación: {{$post->created_at}}</li>
 </ul>
